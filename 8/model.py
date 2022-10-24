068cfba912d7f4cacb20bbfc32088b9aa8e7b8e2
Igor Leonovich <igorleo55@rambler.ru>
2022-10-24 18:57:14 +0300

    ДЗ8

---
+++ 8/model.py
@@ -0,0 +1,51 @@
+def add_employees():
+    id = input('Введите ID: ')
+    name = input('Введите имя: ')
+    firstName = input('Введите фамилию: ')
+    phone = input('Введите номер телефона: ')
+    post = input('Введите должность: ')
+    salary = input('Введите заработную плату: ')
+    directory = id + ' ' + name + ' ' + firstName + ' ' + phone + ' ' + post + ' ' + salary
+    return directory
+
+def find_employees():
+    return search(input('Введите данные для поиска: '))
+
+def changed_employees(str):
+    strList = str.split()
+    print('Что бы Вы хотели изменить? ')
+    print('1. ID\n2. Имя\n3. Фамилию\n4. Номер телефона\n5. Должность\n6. Заработную плату')
+    mode = input()
+    newData = input('Новое значение => ')
+    if mode == '1':
+        strList[0] = newData
+    elif mode == '2':
+        strList[1] = newData
+    elif mode == '3':
+        strList[2] = newData
+    elif mode == '4':
+        strList[3] = newData
+    elif mode == '5':
+        strList[4] = newData
+    elif mode == '6':
+        strList[5] = newData
+    return " ".join(strList)
+
+def search(a):
+    with open('employees.txt', 'r', encoding='utf-8') as f:
+        return [i.strip() for i in f.readlines() if a in i]
+
+def changes_employees():
+    mass = search('')
+    findC = find_employees()
+    while findC == []:
+        print('Ничего не найдено')
+        findC = find_employees()
+    if len(findC) > 1:
+        print('Найденно несколько значений: ')
+        for i in findC: print(i)
+        findC = [findC[int(input('Введите номер нужного => ')) - 1]]
+    for i in range(len(mass)):
+        if mass[i] == findC[0]:
+            mass[i] = changed_employees(findC[0])
+    return mass
---
+++ 8/ui.py
@@ -0,0 +1,22 @@
+# Создать иформационную систему позволяющую работать с сотрудниками некой компании \ студентами вуза \ учениками школы.
+
+from model import add_employees, find_employees, changes_employees, search
+from logger import write_employees, read_employees, write_re
+
+print('Выберите режим работы с данными сотрудников: ')
+print('1. Внести данные нового сотрудника\n2. Поиск данных сотрудника по базе\n3. Вывести всех сотрудников на экран\n4. Внести изменения')
+mode = input()
+if mode == '1':
+    write_employees(add_employees())
+
+elif mode == '2':
+    for i in find_employees(): print(i)
+
+elif mode == '3':
+    for i in search(''): print(i)
+
+elif mode == '4':
+    write_re(changes_employees())
+
+else:
+    print('Введено неверное значение!')
